ab6b84f324a2fb73fb1d7e3e5189cd3ecf37ea49
Kacper <50482739+Kacper777777@users.noreply.github.com>
2022-01-04 16:20:48 +0100

    Refactor data preprocessing utils. Use pandas DataFrames in the future, create smaller number of files.

--- data_preprocessing/utils.py
+++ data_preprocessing/utils.py
@@ -1,44 +1,55 @@
+import csv
 import numpy as np
 import os
 import chess
 import chess.pgn
 
 
-def create_dataset_from_pgn(pgn_file_path, destination_dir, cond_func, **kwargs):
-    """ Function that reads pgn files one by one
-        based on assumption that every pgn contains exactly two empty lines.
-        Calls check_elo() on every pgn
-
-        Parameters:
-            pgn_file_path (str): path to input file
-            destination_dir (str): where to save generated dataset
-            cond_func: pointer to the function that takes a pgn and returns bool
-    """
+def separate_pgns_into_different_files(pgn_file_path, destination_dir):
+    """Get all the pgns from the single file and write each .pgn into separate file.
+    All of the generated files will be created in the destination dir."""
     with open(pgn_file_path) as input_file:
         counter = 0
         while True:
             game = chess.pgn.read_game(input_file)
             if game is None:
                 break
-            if not cond_func(game, **kwargs):
-                continue
-            game_dir = os.path.join(destination_dir, f'game{counter}')
-            os.mkdir(game_dir)
-            with open(os.path.join(game_dir, "game.pgn"), "w") as one_game_file:
-                print(game, file=one_game_file)
+            with open(os.path.join(destination_dir, f"game{counter}.pgn"), 'w') as out:
+                print(game, file=out)
             counter += 1
-            board = game.board()
-            for actual_index, actual_move in enumerate(game.mainline_moves()):
-                for legal_index, legal_move in enumerate(board.legal_moves):
-                    if legal_move == actual_move:
-                        continue
-                    board.push(legal_move)  # add move
-                    with open(os.path.join(game_dir, f"pos{actual_index}_legal{legal_index}.txt"), "w") as data_file:
-                        print(board.fen(), file=data_file)
-                    board.pop()  # undo move
-                board.push(actual_move)  # add correct move
-                with open(os.path.join(game_dir, f"pos{actual_index}.txt"), "w") as data_file:
-                    print(board.fen(), file=data_file)
+
+
+def create_dataset_from_pgns(pgn_dir, destination_dir, cond_func, **kwargs):
+    """ Function that reads pgn files one by one
+        based on assumption that every pgn contains exactly two empty lines.
+
+        Parameters:
+            pgn_dir (str): path to the directory containing .pgn files
+            destination_dir (str): where to save generated dataset
+            cond_func: pointer to the function that takes a pgn and returns bool
+    """
+    pgn_id = 0
+    for pgn_entry in os.scandir(pgn_dir):
+        pgn_file_path = pgn_entry.path
+        with open(pgn_file_path) as input_file:
+            game = chess.pgn.read_game(input_file)
+            fen_before = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+            if cond_func(game, **kwargs):
+                with open(os.path.join(f"{destination_dir}", f"game{pgn_id}.txt"), "w", newline='') as out_file:
+                    tsv_writer = csv.writer(out_file, delimiter='\t')
+                    tsv_writer.writerow(["FEN before", "FEN after", "PGN ID"])
+                    pgn_id += 1
+                    board = game.board()
+                    for actual_index, actual_move in enumerate(game.mainline_moves()):
+                        for legal_index, legal_move in enumerate(board.legal_moves):
+                            if legal_move == actual_move:
+                                continue
+                            board.push(legal_move)  # make move
+                            tsv_writer.writerow([fen_before, board.fen(), pgn_id])
+                            board.pop()  # undo move
+                        board.push(actual_move)  # add correct move
+                        tsv_writer.writerow([fen_before, board.fen(), pgn_id])
+                        fen_before = board.fen()
 
 
 def convert_fen_to_matrix(fen):
